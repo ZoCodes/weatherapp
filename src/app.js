b5bce40003e80c2885dc5cc8bd487feb241e04ca
ZoCodes <zoeblogg91@gmail.com>
2021-08-21 10:12:23 +0100

    Revert "Shorten forecast day names"
    This reverts commit cd9d51a4288ba65ceb172584b2ea0a01bac24421.

--- src/app.js
+++ src/app.js
@@ -53,7 +53,15 @@ formatDate(theDate);
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   return days[day];
 }
