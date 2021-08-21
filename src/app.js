cd9d51a4288ba65ceb172584b2ea0a01bac24421
ZoCodes <zoeblogg91@gmail.com>
2021-08-21 10:11:56 +0100

    Shorten forecast day names
    Made shorter as didn't fit on mobile

--- src/app.js
+++ src/app.js
@@ -53,15 +53,7 @@ formatDate(theDate);
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
   return days[day];
 }
